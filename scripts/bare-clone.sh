02823c3cc127fab7bfae5e9a06351e6f9451c47e
Maxime Mangel <me@mangelmaxime.fr>
2025-07-08 16:26:58 +0200

    feat: [bare-clone.sh] check if destination is empty, init with a scratchpad worktree

--- scripts/bare-clone.sh
+++ scripts/bare-clone.sh
@@ -8,53 +8,59 @@ if [[ "${BASH_SOURCE[0]}" == "${0}" ]]; then
     exit 1
 fi
 
+# We can't use exit to do early return, because it will exit the parent shell
+# Because the script is sourced instead of executed
 if [ -z "$1" ]; then
-  echo "Usage: $0 <repository-url>"
-  exit 1
+    echo "Usage: . bare-clone <repository-url>"
+    return 1
+else
+    getProvider () {
+    case "$1" in
+        *git@github.com*)
+            echo "Github"
+            ;;
+        *)
+        echo "❌ Unsupported git provider. Only Github is supported."
+        exit 1
+            ;;
+        esac
+
+        return $result
+    }
+
+    # git@github.com:whitetigle/vercel-mg-dalkia.git
+    getOrg () {
+        repo=$(echo "$1" | cut -d':' -f2)
+        username=$(echo "$repo" | cut -d'/' -f1)
+        echo "$username"
+    }
+
+    getRepoName () {
+        repo=$(echo "$1" | cut -d':' -f2)
+        repo_name=$(basename "$repo" .git)
+        echo "$repo_name"
+    }
+
+    git_provider=$(getProvider "$1")
+    git_org=$(getOrg "$1")
+    git_repo_name=$(getRepoName "$1")
+
+    destination="$workspaces_path/$git_provider/$git_org/$git_repo_name"
+
+    # Check if the repository already exists
+    if [ -d "$destination" ]; then
+    echo "Repository already exists at $destination"
+    exit 0
+    fi
+
+    # Ensure the destination directory exists
+    mkdir -p "$destination"
+
+    cd "$destination"
+    git clone --bare "$1"
+    cd "$git_repo_name.git"
+
+    # Create a worktree for scratchpad (useful for testing PR or quickly testing changes)
+    git worktree add ../scratchpad
+    return 0
 fi
-
-getProvider () {
-  case "$1" in
-    *git@github.com*)
-        echo "Github"
-        ;;
-    *)
-      echo "❌ Unsupported git provider. Only Github is supported."
-      exit 1
-        ;;
-    esac
-
-    return $result
-}
-
-# git@github.com:whitetigle/vercel-mg-dalkia.git
-getOrg () {
-    repo=$(echo "$1" | cut -d':' -f2)
-    username=$(echo "$repo" | cut -d'/' -f1)
-    echo "$username"
-}
-
-getRepoName () {
-    repo=$(echo "$1" | cut -d':' -f2)
-    repo_name=$(basename "$repo" .git)
-    echo "$repo_name"
-}
-
-git_provider=$(getProvider "$1")
-git_org=$(getOrg "$1")
-git_repo_name=$(getRepoName "$1")
-
-destination="$workspaces_path/$git_provider/$git_org/$git_repo_name"
-
-# Check if the repository already exists
-if [ -d "$destination" ]; then
-  echo "Repository already exists at $destination"
-  exit 0
-fi
-
-# Ensure the destination directory exists
-mkdir -p "$destination"
-
-cd "$destination"
-git clone --bare "$1"
-cd "$git_repo_name.git"
